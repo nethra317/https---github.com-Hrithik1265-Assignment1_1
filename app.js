59afdeb92ec171ceb0609c62ada86ae5625c57b1
nethra317 <nethravathi3172001@gmail.com>
2023-12-10 20:46:32 +0530

    third commit

--- app.js
+++ app.js
@@ -1,52 +1,110 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = 3000
-app.use(bodyParser.json());
+const PORT = 4000
+app.use(express.json());
 
-app.get('/', (request, response) => {
-         response.json({Welcome: 'Welcome to the port 3000' })
-})
-     
-// Create a new subject
-app.post('/subjects', async (req, res) => {
-  const { description, subjectMasterId, chapters, semesters } = req.body;
-
+// Create Subject API
+app.post('/api/v1/hod/subject', async (req, res) => {
   try {
+    const { subject_name, category, sub_category, discipline, level, subject } = req.body;
     const newSubject = await prisma.subject.create({
       data: {
-        description,
-        subjectMaster: { connect: { id: subjectMasterId } },
-        chapters,
-        semesters,
+        description: subject_name,
+        subjectMaster: {
+          create: {
+            category,
+            subCategory: sub_category,
+            dicipline: discipline,
+            level,
+            subName: subject,
+          },
+        },
       },
     });
 
-    res.json(newSubject);
+    res.json({ sts: true, msg: 'Subject added' });
   } catch (error) {
-    res.status(500).json({ error: 'Unable to create subject' });
+    console.error('Error creating subject:', error);
+    res.status(500).json({ sts: false, msg: 'Failed to create subject' });
   }
 });
 
-// Delete a subject by ID
-app.delete('/subjects/:id', async (req, res) => {
-  const subjectId = req.params.id;
-
+// Get Subject API
+app.get('/api/v1/hod/subject', async (req, res) => {
   try {
-    const deletedSubject = await prisma.subject.delete({
-      where: { id: subjectId },
+    const subjects = await prisma.subject.findMany({
+      include: {
+        subjectMaster: true,
+      },
     });
 
-    res.json(deletedSubject);
+    res.json({ sts: true, data: { count: subjects.length, results: subjects } });
   } catch (error) {
-    res.status(500).json({ error: 'Unable to delete subject' });
+    console.error('Error getting subjects:', error);
+    res.status(500).json({ sts: false, msg: 'Failed to get subjects' });
   }
 });
 
-// Start the server
-app.listen(PORT, () => {
-         console.log(`Server has been Started at port ${PORT}`);
+// Delete Subject API
+app.delete('/api/v1/hod/group/subject', async (req, res) => {
+  try {
+    const { subject_ids } = req.body;
+    
+        // Check if subjects are active before deleting
+        const activeSubjects = await prisma.subject.findMany({
+          where: {
+            id: {
+              in: subject_ids,
+            },
+            status: true, // Assuming 'status' represents the active status of subjects
+          },
+        });
+    
+        if (activeSubjects.length > 0) {
+          return res.json({ sts: false, msg: "Can't delete active subjects" });
+        }
+
+    await prisma.subject.deleteMany({
+      where: {
+        id: {
+          in: subject_ids,
+        },
+      },
+    });
+
+    res.json({ sts: true, msg: 'Subjects deleted' });
+  } catch (error) {
+    console.error('Error deleting subjects:', error);
+    res.status(500).json({ sts: false, msg: 'Failed to delete subjects' });
+  }
 });
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+//POST :
+//https://api.iqlabsacademy.com/424f_0659/api/v1/hod/subject/
+//{
+//   "subject_name": "Math 2",
+//   "category": "DEG",
+//   "sub_category": "ENG",
+//   "discipline": "CS",
+//   "level": "002",
+//   "subject": "SOM"
+// }
+
+//GET
+//https://api.iqlabsacademy.com/424f_0659/api/v1/hod/subject/.
+
+
+//DELETE
+//https://api.iqlabsacademy.com/424f_0659/api/v1/hod/group/subject/
+// {
+//   "subject_ids": ["656c2438e6c10b997d06c0e3"]
+// }
